749479c4a2fae69707b84b9657b395bb50cdb52c
kingdog0220 <kingdog0220@gmail.com>
2025-08-09 14:27:21 +0900

    ユーザーエンティティ修正

--- BlazorWasmTemplate.Domain/Entities/User.cs
+++ BlazorWasmTemplate.Domain/Entities/User.cs
@@ -1,9 +1,28 @@
 namespace BlazorWasmTemplate.Domain.Entities
 {
+    /// <summary>
+    /// ユーザー
+    /// </summary>
     public class User
     {
+        /// <summary>
+        /// 物理ID
+        /// </summary>
         public Guid Id { get; set; }
+
+        /// <summary>
+        /// コード
+        /// </summary>
+        public string Code { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 名称
+        /// </summary>
         public string? Name { get; set; }
+
+        /// <summary>
+        /// 有効/無効フラグ
+        /// </summary>
         public bool IsActive { get; set; }
     }
 }
