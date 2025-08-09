2036ac5f1d69a8d9d8d154af4832f96627fa184e
kingdog0220 <kingdog0220@gmail.com>
2025-08-09 14:44:25 +0900

    ユーザー作成マイグレーションファイル作成

---
+++ BlazorWasmTemplate.Migrations/Migrations/20250809053938_CreateUser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorWasmTemplate.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "tpl");
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                schema: "tpl",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    code = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    isactive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_code",
+                schema: "tpl",
+                table: "users",
+                column: "code",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users",
+                schema: "tpl");
+        }
+    }
+}
