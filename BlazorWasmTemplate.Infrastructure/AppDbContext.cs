c016f6faf1c34895cd54cfee397c6a803fecaa0d
kingdog0220 <kingdog0220@gmail.com>
2025-08-09 13:56:16 +0900

    マイグレーション実行のための調整

--- BlazorWasmTemplate.Infrastructure/AppDbContext.cs
+++ BlazorWasmTemplate.Infrastructure/AppDbContext.cs
@@ -12,6 +12,28 @@ namespace BlazorWasmTemplate.Infrastructure
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.HasDefaultSchema("tpl");
+
+            // すべてのエンティティのテーブル名を小文字に変換
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                // テーブル名を小文字に設定
+                var tableName = entity.GetTableName();
+                if (tableName != null)
+                {
+                    entity.SetTableName(tableName.ToLower());
+                }
+
+                // カラム名も小文字にする
+                foreach (var property in entity.GetProperties())
+                {
+                    if (property != null)
+                    {
+                        property.SetColumnName(property.GetColumnName().ToLower());
+                    }
+                }
+            }
+
             // Fluent API設定が必要であればここで行う
         }
     }
