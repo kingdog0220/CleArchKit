95d139fa051d7a6222c73e87bddda5b240f71cb5
kingdog0220 <kingdog0220@gmail.com>
2025-09-13 11:26:08 +0900

    ユーザー更新画面の追加

--- CleArchKit.Application/Users/Dtos/UpdateUserDto.cs
+++ CleArchKit.Application/Users/Dtos/UpdateUserDto.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using CleArchKit.Domain.Users.Entities;
 
 namespace CleArchKit.Application.Users.Dtos
@@ -15,11 +16,14 @@ namespace CleArchKit.Application.Users.Dtos
         /// <summary>
         /// コード
         /// </summary>
+        [Required(ErrorMessage = "コードは必須です。")]
+        [StringLength(10, ErrorMessage = "コードは10文字以内で入力してください。")]
         public string Code { get; set; } = string.Empty;
 
         /// <summary>
         /// 名称
         /// </summary>
+        [StringLength(50, ErrorMessage = "名称は50文字以内で入力してください。")]
         public string? Name { get; set; }
 
         /// <summary>
